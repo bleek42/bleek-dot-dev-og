a26dff927f034b68ab15b51ee269bcfe94e7e426
bleek42 <brandonleek42@gmail.com>
2020-09-02 02:12:24 -0400

    more styles, refactoring jsx & styles in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export default function Projects() {
   return (
-    <div>
-      <section id="project-1">
-        <title id="section-titles">Multiple Choice Quiz App</title>
-        My first web application is a simple 5 question quiz, made for anyone
+    <div className="projects">
+      <section id="project">
+        <h4>Multiple Choice Quiz App</h4>
+        <p>My first web application is a simple 5 question quiz, made for anyone
         that enjoys simple bar trivia and wants to test their knowledge of
         worldly, yet relatively basic facts. This application keeps track of the
         users score, displays whether the user submitted a correct or incorrect
         answer after every question and allows the user to reset the quiz from
         the beginning upon completion. This application utilizes the widely
         deployed Javascript jQuery library, which is used in more than half of
-        the top 1 million websites in the world!{' '}
+        the top 1 million websites in the world!</p>
         <p>
-          You can take the quiz yourself{' '}
+          You can take the quiz yourself
           <a
             href="https://thinkful-ei-macaw.github.io/quizapp_DB/"
             target="_blank"
@@ -47,15 +47,15 @@ export default function Projects() {
           height="300"
         />
       </section>
-      <section id="project-2">
-        <title id="section-titles">Bookmarks App</title>
-        My second web application is a Bookmarks application, that also utilizes
+      <section id="project">
+        <h4>Bookmarks App</h4>
+        <p>My second web application is a Bookmarks application, that also utilizes
         jQuery and a public API that makes post, update, and delete requests. It
         also has an expand/collapse function to view details of individual
         bookmarks, validates user entry forms when adding a new bookmark, and a
         rating function from 1 to 5 that allows you to filter by your bookmarks
         rating. The API functionality allows the user to access them from any
-        device, as any bookmarks app should!
+        device, as any bookmarks app should!</p>
         <p>
           You can try it out for yourself{' '}
           <a
@@ -77,11 +77,11 @@ export default function Projects() {
           height="500"
         />
       </section>
-      <section id="project-3">
-        <title id="section-titles">
+      <section children="project">
+        <h4>
           Coronavirus in the USA - A Message Board
-        </title>
-        Furthering my skills to full-stack programming with more modern
+        </h4>
+        <p>Furthering my skills to full-stack programming with more modern
         JavaScript tools, my third effort is an anonymous message board on the
         subject of the COVID-19 outbreak in the United States. Using React.js
         for client-side and Node.js for server-side, I created a small platform
@@ -91,7 +91,7 @@ export default function Projects() {
         they choose, select 1 of 54 states/territories, and their most recent
         occupation. From there, they are directed to the state they can leave
         their thoughts, see what others commented, and view the current total
-        cases and fatalities relative to that state.
+        cases and fatalities relative to that state.</p>
         <p>
           You can try it out for yourself{' '}
           <a href="https://corona-message-board-us.now.sh//" target="_blank">
@@ -114,11 +114,11 @@ export default function Projects() {
           height="500"
         />
       </section>
-      <section id="project-4">
-        <title id="section-titles">
+      <section id="project">
+        <h4>
           Spaced Repetition & The Spanish Language
-        </title>
-        My next full-stack effort using React and Node explores the learning
+        </h4>
+        <p>My next full-stack effort using React and Node explores the learning
         technique known as spaced-repetition. Spaced-repetition is exactly how
         it sounds: repeating something in timed intervals, in an effort to
         commit to memory new concepts and abilities one wants to learn.
@@ -126,7 +126,7 @@ export default function Projects() {
         algoritihm that serves as a score multiplier, 256-bit encryption with
         JSON WebToken for secure user registration & login, and a modern user
         interface with SASS style sheets. Please note that this was a joint
-        effort between a fellow student and I.
+        effort between a fellow student and I.</p>
         <p>
           You can try it out for yourself{' '}
           <a
@@ -140,8 +140,7 @@ export default function Projects() {
             here!
           </a>{' '}
           The server source code can be found{' '}
-          <a href="https://github.com/thinkful-ei-macaw/spaced-repetition-api-Brandon-Sara">
-            here!
+          <a href="https://github.com/thinkful-ei-macaw/spaced-repetition-api-Brandon-Sara" rel="noopener noreferrer" target="_blank">here!
           </a>{' '}
         </p>
         <img
@@ -152,16 +151,16 @@ export default function Projects() {
           height="500"
         />
       </section>
-      <section id="project-5">
-        <title id="section-titles">Trouvaille</title>
-        My latest project is a travel planner application with a React & SASS
+      <section className="project">
+        <h4>Trouvaille</h4>
+        <p>My latest project is a travel planner application with a React & SASS
         user-interface, Google Maps integration, and a secure Node server that
         verifies & sends emails. As a user, you can plan customizable
         cross-country trips according to your interests. Users select their
         origin, destination, and categories (landmarks, recreation, etc.),
         providing places you can visit along your way. Other features include
         secure registration & login, email verification & password reset with
-        Nodemailer, and an animated loading screen.
+        Nodemailer, and an animated loading screen.</p>
         <p>
           You can try it out for yourself{' '}
           <a
@@ -187,6 +186,6 @@ export default function Projects() {
           height="500"
         />
       </section>
-    </div>
+    </div >
   );
 }
