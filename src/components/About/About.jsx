e262a660084ccaddf0d427835b004b1de099682e
bleek42 <brandonleek42@gmail.com>
2020-10-07 22:09:23 -0400

    fixed bottom margin / padding in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,51 +1,51 @@
-import React from "react";
-import { VscSymbolArray } from "react-icons/vsc";
+import React from 'react';
+import { VscSymbolArray } from 'react-icons/vsc';
 
-import "./About.scss";
+import './About.scss';
 
 export default function About() {
-  return (
-    <div className="about">
-      <section>
-        <h3>{<VscSymbolArray />} About</h3>
-        <p>
-          My name is Brandon Leek and I'm Junior Full-Stack Web Developer,
-          experienced salesman and avid live music fan!
-        </p>
-        <p>
-          I graduated from Thinkful's Full-Stack Engineering Immersion course in
-          the Summer of 2020, and found an exciting new passion in programming.
-          I've been furthering my learning by contributing to personal and
-          open-source projects almost daily since, as I search for my place in
-          the tech industry. Looking forward to picking up a second programming
-          language one day: leaning towards something closer to the hardware,
-          like C++ or Rust, but remaining open to where I go next in my journey!
-        </p>
-        <p>
-          Until then, my area of focus is React web applications & Node.js
-          server applications, alongside various libraries & frameworks, such as
-          TypeScript, Material UI, GraphQL, Puppeteer.js, and numerous others.
-          I'm always learning something new and very active in helping others in
-          my Slack & Discord groups.
-        </p>
-        <p>
-          I've been using computers since I was in single-digits: from modding
-          computer games, tinkering with HTML in the days of MySpace.com,
-          installing custom OS's on my android phones, and now testing +
-          deploying fully functional web clients & servers. Prior experience in
-          my professional & personal life have given me a vested interest in the
-          direction information technology takes, having been in client-facing
-          technology sales and of course being a client myself!
-        </p>
-        <p>
-          In my leisure time, I love to travel the country to large scale
-          events, particularly music festivals! I've been all over the country
-          and have even volunteered my time at a few, ensuring attendees and
-          friends alike have the time of time of their lives and remain safe.
-          It's a fun, collaborative culture that I am proud to call myself a
-          part of.
-        </p>
-      </section>
-    </div>
-  );
+	return (
+		<div className="about">
+			<section>
+				<h3>{<VscSymbolArray />} About</h3>
+				<p>
+					My name is Brandon Leek and I'm Junior Full-Stack Web Developer,
+					experienced salesman and avid live music fan!
+				</p>
+				<p>
+					I graduated from Thinkful's Full-Stack Engineering Immersion course in
+					the Summer of 2020, and found an exciting new passion in programming.
+					I've been furthering my learning by contributing to personal and
+					open-source projects almost daily since, as I search for my place in
+					the tech industry. Looking forward to picking up a second programming
+					language one day: leaning towards something closer to the hardware,
+					like C++ or Rust, but remaining open to where I go next in my journey!
+				</p>
+				<p>
+					Until then, my area of focus is React web applications & Node.js
+					server applications, alongside various libraries & frameworks, such as
+					TypeScript, Material UI, GraphQL, Puppeteer.js, and numerous others.
+					I'm always learning something new and very active in helping others in
+					my Slack & Discord groups.
+				</p>
+				<p>
+					I've been using computers since I was in single-digits: from modding
+					computer games, tinkering with HTML in the days of MySpace.com,
+					installing custom OS's on my android phones, and now testing +
+					deploying fully functional web clients & servers. Prior experience in
+					my professional & personal life have given me a vested interest in the
+					direction information technology takes, having been in client-facing
+					technology sales and of course being a client myself!
+				</p>
+				<p>
+					In my leisure time, I love to travel the country to large scale
+					events, particularly music festivals! I've been all over the country
+					and have even volunteered my time at a few, ensuring attendees and
+					friends alike have the time of time of their lives and remain safe.
+					It's a unique, collaborative culture that I am proud to call myself a
+					part of.
+				</p>
+			</section>
+		</div>
+	);
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,80 +8,73 @@ import { API_TOKEN, API_URL } from '../../api-config';
 import './Contact.scss';
 
 function Contact() {
-  const [profile, setProfile] = useState({});
+	const [profile, setProfile] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const req = {
-          Authorization: `Bearer ${API_TOKEN}`,
-        };
-        const res = await fetch(API_URL, req);
-        const data = await res.json();
-        console.table(data);
-        setProfile(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const req = {
+					Authorization: `Bearer ${API_TOKEN}`,
+				};
+				const res = await fetch(API_URL, req);
+				const data = await res.json();
+				setProfile(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, [profile]);
 
-  return (
-    <div className="contact">
-      <header>
-        <h4>{<VscCommentDiscussion />} Contact</h4>
-        <p>
-          Whether you're about to make me an offer I can't refuse, thinking
-          about becoming a developer, or somewhere in between: I'd love to
-          connect if you're taking the time to read this!
-        </p>
-      </header>
-      <section className="email">
-        <h4>{<HiOutlineMail />} Email</h4>
-        <a
-          id="my-email"
-          href="mailto:brandonleek42@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          brandonleek42@gmail.com
-        </a>
-      </section>
-      <section className="linkedin">
-        <h4>{<GrLinkedin />} LinkedIn</h4>
-        <a
-          id="my-linkedin"
-          href="https://www.linkedin.com/in/brandon-leek-4031891a4/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to view my profile!
-        </a>
-      </section>
-      <section className="github">
-        <h4>{<FiGithub />} Github</h4>
-        <a
-          id="my-github"
-          key={profile.html_url}
-          href={profile.html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to view my profile!
-        </a>
-        <p key={profile.public_repos}>
-          Total Repos: {profile.public_repos}
-        </p>
-        <p key={profile.followers}>
-          Total Followers: {profile.followers}
-        </p>
-        <p key={profile.following}>
-          Total Followiing: {profile.following}
-        </p>
-      </section>
-    </div>
-  );
+	return (
+		<div className="contact">
+			<header>
+				<h4>{<VscCommentDiscussion />} Contact</h4>
+				<p>
+					Whether you're about to make me an offer I can't refuse, thinking
+					about becoming a developer, or somewhere in between: I'd love to
+					connect if you're taking the time to read this!
+				</p>
+			</header>
+			<section className="email">
+				<h4>{<HiOutlineMail />} Email</h4>
+				<a
+					id="my-email"
+					href="mailto:brandonleek42@gmail.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					brandonleek42@gmail.com
+				</a>
+			</section>
+			<section className="linkedin">
+				<h4>{<GrLinkedin />} LinkedIn</h4>
+				<a
+					id="my-linkedin"
+					href="https://www.linkedin.com/in/brandon-leek-4031891a4/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Click here to view my profile!
+				</a>
+			</section>
+			<section className="github">
+				<h4>{<FiGithub />} Github</h4>
+				<a
+					id="my-github"
+					key={profile.html_url}
+					href={profile.html_url}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Click here to view my profile!
+				</a>
+				<p key={profile.public_repos}>Total Repos: {profile.public_repos}</p>
+				<p key={profile.followers}>Total Followers: {profile.followers}</p>
+				<p key={profile.following}>Total Followiing: {profile.following}</p>
+			</section>
+		</div>
+	);
 }
 
 export default Contact;
